0efb890a0e60bc062d215892a71f364c7f262ada
parasquid <tristan.gomezd@gmail.com>
2012-02-03 04:31:55 +0800

    this is the frontend for now

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,14 @@
 class Profile
   include Mongoid::Document
+
+  default_scope asc(:order)
+
   field :name, type: String
   field :avatar_url, :type => String
   field :title, type: String
   field :description, :type => String
   field :email, type: String
+  field :order, type: Integer
 
   include Mongoid::Slug
   slug :name
