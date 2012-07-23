9c53a073b05dcb1f8eed4b1ed02aac7768618206
Tristan <tristan.gomez@gmail.com>
2012-07-23 20:50:07 +0800

    make sure correct values are passed

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -5,7 +5,7 @@ class QuestionMailer < ActionMailer::Base
     @email = msg[:email]
     @body = msg[:body]
     @name = msg[:name]
-    mail(to: @email, subject: '[Ask@Mindvalley] Someone asked you a question ...')
+    mail(to: @email, subject: msg[:subject] || '[Ask@Mindvalley] Someone asked you a question ...')
     puts "question sent to #{@email}"
   end
 end
