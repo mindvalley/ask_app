982ca8e37c8494c61562f3e232044d0170efeee5
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-09-06 13:03:39 +0800

    database settings

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AskMindvalley::Application.config.secret_key_base = '1985753858feb21cfdf5ff867a8219f0a92d7b0925c2358c27f6f79265236cdc6bc03f67994d0e521e7e66dbce94a84b8c8088ad67ef97e15cef7aabb833fc25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AskMindvalley::Application.config.session_store :cookie_store, key: '_ask@mindvalley_session'
