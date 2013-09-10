96799d7ba092616a259d905737d10be940a90cb2
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-09-10 19:05:20 +0800

    server configs

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/mvdev/apps/ask@mindvalley/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.blog.sock"
+worker_processes 8
+timeout 30
