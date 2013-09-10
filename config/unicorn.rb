01932dafb9a10c46f49149c834069016afe1f256
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-09-10 20:08:09 +0800

    fixed server config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 root = "/home/mvdev/apps/ask@mindvalley/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "#{root}/tmp/pids/unicorn1.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
-worker_processes 8
+listen "/tmp/unicorn.blog1.sock"
+worker_processes 2
 timeout 30
