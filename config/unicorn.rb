bd1324145b250602f2b2087ac9ad6508ac86e605
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-09-12 18:59:34 +0800

    unicorn socket changed

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 root = "/home/mvdev/apps/ask@mindvalley/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn1.pid"
+pid "#{root}/tmp/pids/unicorn2.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog1.sock"
+listen "/tmp/unicorn.blog2.sock"
 worker_processes 2
 timeout 30
