6945993aac4507f702eab8842c586248f01b43c2
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-09-27 15:14:50 +0800

    custom text extraction and parser

--- app/assets/javascripts/polls.js
+++ app/assets/javascripts/polls.js
@@ -1,6 +1,11 @@
+var myTextExtraction = function(node)  
+{  
+    // extract data from markup and return it  
+    return $(node.childNodes[1].childNodes[5]).text(); 
+}
 $(document).ready(function(){
 	$('#close').click(function(){
 		$("#notification").hide('slow');
 	});
-	$(".mws-table").tablesorter();
+	$(".mws-table").tablesorter({textExtraction: myTextExtraction});
 });
